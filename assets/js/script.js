5e9d08f883421d1878009b3c97e662f7ed23e44c
Carlos Pino <carlosgenaropinoromero@gmail.com>
2024-03-28 23:43:18 -0300

    añadiendo funciones click(areaFontAwesome) + dblclickclick(areaFontAwesome) + alert(botonFormulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,28 @@ document.getElementById('enviarFormulario').onclick = function () {
     alert("El correo fue enviado correctamente");
 }
 
+
+document.getElementById("varicolor").addEventListener("click", function () {
+    this.style.backgroundColor = "red";
+});
+
+
+document.getElementById("varicolor2").addEventListener("click", function () {
+    this.style.backgroundColor = "red";
+});
+
+document.getElementById("varicolor3").addEventListener("click", function () {
+    this.style.backgroundColor = "red";
+});
+
+document.getElementById("varicolor").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "aqua";
+});
+
+document.getElementById("varicolor2").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "aqua";
+});
+
+document.getElementById("varicolor3").addEventListener("dblclick", function () {
+    this.style.backgroundColor = "aqua";
+});
